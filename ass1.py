4844fdc4836e1289be2c479cc5f7d122029d7fef
Sujeet1249 <paswansujeet1249@gmail.com>
2026-02-18 23:21:31 +0530

    Delete ass1.py

--- ass1.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Programing Language Peekr App")
-st.subheader("Welcom to our App")
-
-programing = st.selectbox("Choose your favriote Programing Language: ", ["C", "C++", "Python", "Javascript", "Java", "React"])
-st.write(f"You choose {programing} langauge.")
-
-st.success("Great! Your choice is best.")
